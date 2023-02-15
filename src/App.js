6abfa00ee8cc582d1d989f3b4442bff36d0e33a8
2hongbi <foxgirin950@gmail.com>
2023-02-15 18:55:16 +0900

    Feat: Todo 아이템 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
+import React, { useState } from "react";
+import { List, Paper } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, setItems] = useState([
+        {
+            id: "0",
+            title: "Hello World 1",
+            done: true
+        },
+        {
+            id: "1",
+            title: "Hello World 2",
+            done: true
+        }
+    ]);
+
+    let todoItems = items.length > 0 &&
+        <Paper style={{ margin: 16 }}>
+            <List>
+                {items.map((item) => (
+                    <Todo item={item} key={item.id} />
+                ))}
+            </List>
+        </Paper>
+    return (
+        <div className="App">
+            {todoItems}
+        </div>
+    );
 }
 
 export default App;
